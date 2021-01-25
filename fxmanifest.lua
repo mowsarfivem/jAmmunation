50926d36cf93942191c441ac441a9c8c39fc343f
Jamsou <71193282+Jamsouuuu@users.noreply.github.com>
2021-01-25 23:04:52 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "adamant"
+game "common"
+author "jamsou"
+
+client_scripts{
+    "dependencies/pmenu.lua",
+    "client/ammu_cl.lua",
+}
+
+server_scripts{
+    "server/ammu_sv.lua",
+}
